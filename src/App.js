b2183332af09f0058b988b1fd4e4b05258bcfb88
DarkoKrezic <darkotronic@gmail.com>
2023-09-22 00:09:59 +0200

    favicon

--- src/App.js
+++ src/App.js
@@ -15,59 +15,111 @@ function App() {
   return (
     <div className="App">
       <nav className="navbar sticky">
-        <ul className="nav"> 
+        <ul className="nav">
           <li className="nav-item">
-            <a className="nav-link" href="#about">About</a>
+            <a className="nav-link" href="#about">
+              About
+            </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#projects">Projects</a>
-          </li>          
-           <li className="nav-item">
-         <a className="nav-link" href="#skills">Skills</a>
+            <a className="nav-link" href="#projects">
+              Projects
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#skills">
+              Skills
+            </a>
           </li>
 
           <li className="nav-item">
-            <a className="nav-link" href="#contact">Contact</a>
+            <a className="nav-link" href="#contact">
+              Contact
+            </a>
           </li>
-          </ul>
+        </ul>
       </nav>
-<h1 className="HiIMDarko">Hi, <img src={profilePic} className="profilePic"  alt="darko profile pic"/> <br/>I’m Darko.</h1> 
-      <div className="container" >
-      <section id="about">
-        <h2 className="section-header">About:</h2>
-       <p className="section-text" >i am a Web Developer, Music producer, Studio and label owner, and a Basketball veteran from Hamburg, Germany. I learned Web Development at NeueFische GmbH by doing a many different projects using modern technologies. I live in Hamburg and I am enthusiastic about the AI field. I am also a music producer with over a decade of experience as well as a veteran professional basketball player . </p>
-      </section>
-      <section id="projects">
-        <h2 className="section-header">Projects:</h2>
-         <a className="project-header" href="https://capstone-projekt-lil-storyteller.vercel.app/" >Lil Storyteller</a>
-         <p className="section-text" > This is an App that allows you to create your own story using AI</p>
-        <a className="project-header" href="https://chefsquizquest.vercel.app/">Chefs Quiz Quest</a>
-    <p className="section-text" >This is an AI powered Quiz. Both projects are open source and hosted on github</p>
-
-      </section>
-      <section id="skills">
-        <h2 className="section-header">Skills:</h2>
-        <div className="skillImageContainer" >
-       <img src={jsIcon} className="skillIcon" alt="jsIcon" />
-       <img src={reactIcon} className="skillIcon" alt="reactIcon" />
-       <img src={mongoIcon} className="skillIcon" alt="mongoIcon" />
-       <img src={nodeIcon} className="skillIcon" alt="nodeIcon" />
-       <img src={npmIcon} className="skillIcon" alt="npmIcon" />
-       <img src={jsonIcon} className="skillIcon" alt="jsonIcon" />
-       <img src={nextIcon} className="skillIcon" alt="nextIcon" />
- </div>
-      </section>
-      <section id="contact">
-        <h2 className="section-header">Contact:</h2>
-        <div className="contact-links">
-        < a href="https://github.com/DarkoKrezic" target="_blank" rel="noreferrer" ><img src={githubIcon} className="skillIcon" alt="github" /> </a>
-        < a href="https://www.linkedin.com/in/darko-krezic/" target="_blank" rel="noreferrer" ><img src={linkedinIcon} className="skillIcon" alt="LinkedIn" /> </a>
-        <a href="https://www.instagram.com/darko_beats_/?hl=de" target="_blank" rel="noreferrer" ><img src={instaIcon} className="skillIcon" alt="Instagram" /> </a>
-       </div>
-      </section>
-      
+      <h1 className="HiIMDarko">
+        Hi,{" "}
+        <img src={profilePic} className="profilePic" alt="darko profile pic" />{" "}
+        <br />
+        I’m Darko.
+      </h1>
+      <div className="container">
+        <section id="about">
+          <h2 className="section-header">About:</h2>
+          <p className="section-text">
+            i am a Web Developer, Music producer, Studio and label owner, and a
+            Basketball veteran from Hamburg, Germany. I learned Web Development
+            at NeueFische GmbH by doing a many different projects using modern
+            technologies. I live in Hamburg and I am enthusiastic about the AI
+            field. I am also a music producer with over a decade of experience
+            as well as a veteran professional basketball player .{" "}
+          </p>
+        </section>
+        <section id="projects">
+          <h2 className="section-header">Projects:</h2>
+          <a
+            className="project-header"
+            href="https://capstone-projekt-lil-storyteller.vercel.app/"
+          >
+            Lil Storyteller
+          </a>
+          <p className="section-text">
+            {" "}
+            This is an App that allows you to create your own story using AI
+          </p>
+          <a
+            className="project-header"
+            href="https://chefsquizquest.vercel.app/"
+          >
+            Chefs Quiz Quest
+          </a>
+          <p className="section-text">
+            This is an AI powered Quiz. Both projects are open source and hosted
+            on github
+          </p>
+        </section>
+        <section id="skills">
+          <h2 className="section-header">Skills:</h2>
+          <div className="skillImageContainer">
+            <img src={jsIcon} className="skillIcon" alt="jsIcon" />
+            <img src={reactIcon} className="skillIcon" alt="reactIcon" />
+            <img src={mongoIcon} className="skillIcon" alt="mongoIcon" />
+            <img src={nodeIcon} className="skillIcon" alt="nodeIcon" />
+            <img src={npmIcon} className="skillIcon" alt="npmIcon" />
+            <img src={jsonIcon} className="skillIcon" alt="jsonIcon" />
+            <img src={nextIcon} className="skillIcon" alt="nextIcon" />
+          </div>
+        </section>
+        <section id="contact">
+          <h2 className="section-header">Contact:</h2>
+          <div className="contact-links">
+            <a
+              href="https://github.com/DarkoKrezic"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={githubIcon} className="skillIcon" alt="github" />{" "}
+            </a>
+            <a
+              href="https://www.linkedin.com/in/darko-krezic/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={linkedinIcon} className="skillIcon" alt="LinkedIn" />{" "}
+            </a>
+            <a
+              href="https://www.instagram.com/darko_beats_/?hl=de"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={instaIcon} className="skillIcon" alt="Instagram" />{" "}
+            </a>
+          </div>
+        </section>
       </div>
-      </div>
+    </div>
   );
 }
 
